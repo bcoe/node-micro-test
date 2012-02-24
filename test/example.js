2e718e0380d7a594e164e2b1b518287bf3d77f51
bcoe <bencoe@gmail.com>
2012-02-24 13:27:22 -0800

    Updated example test.

--- test/example.js
+++ test/example.js
@@ -2,8 +2,8 @@ var equal = require('assert').equal;
 
 exports.tests = {
 	'true should be equal to true': function(finished, prefix) {
-		equal(true, true, prefix + " that's weird, true should really be equal to true.");
 		setTimeout(function() {
+			equal(true, true, prefix + " that's weird, true should really be equal to true.");
 			finished();
 		}, 2000);
 	}
